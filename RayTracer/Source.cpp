6f315088126ff7232f5c99c1d9fa27b4b7848967
Olav Blaak <olav.blaak@student.kuleuven.be>
2021-08-29 18:31:44 +0200

    Working First Image

--- RayTracer/Source.cpp
+++ RayTracer/Source.cpp
@@ -1,18 +1,21 @@
 #include <fstream>
 #include <limits>
 
-#define HEIGHT 16
-#define WIDTH 16
+#define UCHAR_MAX 255
+#define HEIGHT 255
+#define WIDTH 255
 
 int main() {
 	std::ofstream first_image("image.ppm");
-	first_image << "P6\n";
+	int step_height = UCHAR_MAX / HEIGHT;
+	int step_width = UCHAR_MAX / WIDTH;
+	first_image << "P3\n";
 	first_image << HEIGHT << '\n';
 	first_image << WIDTH << '\n';
-	first_image << UCHAR_MAX;
+	first_image << UCHAR_MAX << '\n';
 	for (int i = 0; i < HEIGHT; i++) {
 		for (int j = 0; j < WIDTH; j++) {
-			first_image << 15 * i << ' ' << 15 * j << ' ' << 0 << ' ';
+			first_image << step_height * i << ' ' << step_width * j << ' ' << 0 << ' ';
 		}
 		first_image << '\n';
 	}
